f3d6f04aa5984a81dadefcf82797a6bd74806f8e
𝗜𝗦𝗧𝗞𝗛𝗔𝗥 𝗕𝗔𝗕𝗬 <mdistkhar601@gmail.com>
2025-05-04 15:03:54 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "6435225"
+# -------------------------------------------------------------
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+STRING1 = getenv("STRING_SESSION", None)
+MONGO_URL = getenv("MONGO_URL", None)
+OWNER_ID = int(getenv("OWNER_ID", "7995626429"))
+SUPPORT_GRP = "THUNDER_SUPPORT_ll"
+UPDATE_CHNL = "THUNDERDEVS"
+OWNER_USERNAME = "ll_ISTKHAR_BABY_lll"
